40c4dad9fb5d903711fdec00c3897da4ec99ec6b
Rosie Red <deep.red.rosie.999@gmail.com>
2018-04-03 09:21:14 +0530

    Better understanding of Tversky similarity

--- affine.py
+++ affine.py
@@ -6,28 +6,50 @@ from matplotlib import *
 from pylab import *
 import py_stringmatching
 
-def image_similarity(A, B):
-    result = cv2.matchTemplate(A, B, cv2.TM_CCORR_NORMED)
+def image_similarity(A, B, alpha, beta):
+    #each feature is a pixel
+    # intersection PRODUCT, union MAXIMUM, and subtraction DIFFERENCE operations are
+    #defined as the product, maximum, and difference of RGB
+    #pixel values, respectively. 0 balck to 255 white
+    #reading grayscale images
+    redA = A
+    redB = B
+    where(redA > 127, 1, 0)
+    where(redB > 127, 1 ,0)
+    red_int = redA*redB
+    red_int_fin = numpy.sum(red_int)
+    #print "red_int_fin", red_int_fin
+    red_un = numpy.sum(maximum(redA,redB))
+    #print "red_un", red_un
+    red_subAB = cv2.subtract(redA,redB)
+    red_subAB = numpy.sum(where(red_subAB > 127,1,0))
+    #print "red_subAB", red_subAB
+    red_subBA = cv2.subtract(redB,redA)
+    red_subBA = numpy.sum(where(red_subBA > 127,1,0))
+    #print "red_subBA", red_subBA
+    similarity = red_int_fin/(red_int_fin + alpha*red_subAB + beta*red_subBA)*1.0
+    return float(similarity)
 
-def transform(index, img):
-    '''2*2 identity mirror flip r90 r180 r270'''
-    '''3*3 union intersection xor'''
+def transform(ind, img):
     if ind==0:
         return img
     elif ind==1:
-        horizontal_img = img.copy()
-        horizontal_img = cv2.flip( img, 0 )
-        return horizontal_img
+        #mirror
+        return cv2.flip(img,1)
     elif ind==2:
+        #flip vertical
+        return cv2.flip(img,0)
+    #elif ind==3:
+        #r90
 
 
 def find_answer(question_images):
     for i in question_images:
         for j in question_images:
-            for ind in range(0,6):
-                j[1] = transform(ind, j[1])
-                if i[0]!=j[0]:
-                    print i[0],j[0],image_similarity(i[1],j[1])
+            #for ind in range(0,6):
+            #j[1] = transform(ind, j[1])
+            if i[0]!=j[0]:
+                print i[0],j[0],image_similarity(i[1],j[1],1,1)
 
 
 
